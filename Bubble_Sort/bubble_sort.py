959607280668fd90593a1b457c09e14bda8110d3
gunupati <gunupatimahesh@gmail.com>
2019-08-12 18:27:34 +0530

    added bubble sort

--- Bubble_Sort/bubble_sort.py
+++ Bubble_Sort/bubble_sort.py
@@ -1,15 +1,27 @@
-def bubble_sort(a):
-    for i in range(len(a)):
-        for j in range(0,len(a)-1):
-            if a[j]>a[j+1]:
-                a[j],a[j+1]=a[j+1],a[j]
+def bubbleSort(arr):
+    n = len(arr)
 
-a=[1,4,2,56,3453,5435,43,3543]
-bubble_sort(a)
-print ("Sorted array is:")
-for i in a:
-    print(i)
+    # Traverse through all array elements
+    for i in range(n):
 
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print("Sorted array is:")
+for i in range(len(arr)):
+    print("%d" % arr[i]),
 
 
 # Sorted array is:
